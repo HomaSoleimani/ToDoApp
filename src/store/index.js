e347085ef97be0245bb02dcec0d85c8bd81d25b7
HomaSoleimani <75659311+HomaSoleimani@users.noreply.github.com>
2021-01-04 13:49:21 +0330

    Update index.js
    Comment Redux DevTools

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,8 @@ import { tasks } from "./../reducers/tasks";
 export const store = createStore(
   tasks,
   compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(thunk)
+   // , window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 store.subscribe(() => console.log(store.getState()));
